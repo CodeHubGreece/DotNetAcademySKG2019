65ee9d2a32fc33d86ba296937e10d528a0a2e3aa
Charalampos Karypidis <xaralaboskaripidis@gmail.com>
2019-04-09 18:44:04 +0300

    Add the project for accessing data through ADO.NET

---
+++ Wed 10-3-2019/DataAccess/DataAccess.ADO/Genre.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.ADO
+{
+	/// <summary>
+	/// Represents the Genre
+	/// </summary>
+	public class Genre
+	{
+		/// <summary>
+		/// The id of the entity
+		/// </summary>
+		public int Id { get; set; }
+
+		/// <summary>
+		/// The name of the entity
+		/// </summary>
+		public string Name { get; set; }
+	}
+}
